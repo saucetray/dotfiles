e8c6ba98a0fcb432e96cdd1cb513160d4c23c027
Justin Sostre <justintimetotheparty@gmail.com>
2021-01-19 15:23:14 -0500

    Removed annoying lockfiles and backups

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,6 +37,31 @@
 (load-theme 'material t)
 (global-linum-mode t)
 
+;; BACKUP HANDLING
+;; Copying set so I do not lose creating times
+(setq backup-by-copying t)
+
+;; make backup to a designated dir, mirroring the full path
+
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let* (
+        (backupRootDir "~/.emacs.d/backup/")
+        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
+        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+        )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+  )
+)
+
+(setq make-backup-file-name-function 'my-backup-file-name)
+
+;; LOCKFILES
+;; I do not need them right now
+(setq create-lockfiles nil)
+
 ;; Python Config
 (load-file "~/.emacs.d/python/pyinit.el")
 
